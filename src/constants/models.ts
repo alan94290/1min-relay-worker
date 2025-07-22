a294ee9b6763722680dc94025e30bd43709ef6be
Alan Kuo <40587302+alan94290@users.noreply.github.com>
2025-07-22 17:23:26 +0800

    Update gemini models
    add gemini models
    - gemini-2.5-pro
    - gemini-2.5-flash

--- src/constants/models.ts
+++ src/constants/models.ts
@@ -45,6 +45,8 @@ export const ALL_ONE_MIN_AVAILABLE_MODELS = [
   "gemini-2.5-flash-preview-05-20",
   "gemini-2.5-flash-preview-04-17",
   "gemini-2.5-pro-preview-05-06",
+  "gemini-2.5-flash",
+  "gemini-2.5-pro",
   // MistralAI
   "mistral-large-latest",
   "mistral-small-latest",
